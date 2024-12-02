e7085d67544e7bd9cacbaa3d70bf82758e7df17f
Josh Gaber <joshgaber@gmail.com>
2024-12-02 11:30:22 -0500

    Day 1 complete

--- app/day1/main.rb
+++ app/day1/main.rb
@@ -1,9 +1,31 @@
 module Day1
   class Main
-    def initialize(input); end
+    def initialize(input)
+      @first_list = []
+      @second_list = []
+      input.lines.each do |line|
+        first, second = line.strip.split(/\s+/).map(&:to_i)
+        @first_list << first
+        @second_list << second
+      end
+    end
 
-    def part1; end
+    def part1
+      first_list_sorted = @first_list.sort
+      second_list_sorted = @second_list.sort
 
-    def part2; end
+      (0...first_list_sorted.length).sum do |i|
+        (first_list_sorted[i] - second_list_sorted[i]).abs
+      end
+    end
+
+    def part2
+      second_list_count = @second_list.group_by { _1 }
+      second_list_count.transform_values!(&:count)
+
+      @first_list.sum do |elem|
+        elem * (second_list_count[elem] || 0)
+      end
+    end
   end
 end
