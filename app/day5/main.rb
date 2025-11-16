5f11bedba4aaeb998882702e38faf0880fcba953
Josh Gaber <joshgaber@gmail.com>
2025-11-15 21:08:34 -0500

    Day 5 complete

--- app/day5/main.rb
+++ app/day5/main.rb
@@ -1,9 +1,23 @@
+require_relative 'page_order'
+
 module Day5
   class Main
-    def initialize(input); end
+    def initialize(input)
+      instructions, orders = input.split("\n\n")
+      @instructions = PageOrder.new(instructions)
+      @orders = orders.lines.map { |line| line.split(',').map(&:to_i) }
+    end
 
-    def part1; end
+    def part1
+      matching = @orders.filter { |order| @instructions.matches?(order) }
+      matching.sum { |arr| arr[arr.length / 2] }
+    end
 
-    def part2; end
+    def part2
+      incorrect = @orders.reject { |order| @instructions.matches?(order) }
+
+      incorrect.map! { |arr| @instructions.reorder(arr)[(arr.length - 1) / 2] }
+      incorrect.sum
+    end
   end
 end
---
+++ app/day5/page_order.rb
@@ -0,0 +1,33 @@
+module Day5
+  class PageOrder
+    Instruction = Data.define(:first, :second)
+
+    attr_reader :page_order, :instructions
+    def initialize(input)
+      @instructions = input.lines(chomp: true).to_set { |rule| rule.split('|').map(&:to_i) }
+    end
+
+    def matches?(list)
+      list.combination(2).all? { |combo| @instructions.include?(combo) }
+    end
+
+    def reorder(list)
+      index_map = list.each_index.to_a.combination(2)
+      list = list.dup
+      loop do
+        break if catch(:sorted) do
+          index_map.each do |i, j|
+            next unless @instructions.include?([list[j], list[i]])
+            list[i], list[j] = list[j], list[i]
+            throw :sorted, false
+          end
+
+          true
+        end
+      end
+      list
+    end
+
+    private
+  end
+end
